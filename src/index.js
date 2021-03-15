9939784e4d75f11d8c1c2cf2d1cd5edd91b10ce1
dimonarhipon <40203705+dimonarhipon@users.noreply.github.com>
2021-03-15 10:56:53 +0300

    init: start reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    const items = Array.from(Math.abs(n).toString());
+    const elements = items.slice();
+    if (items.length > 1) {
+      items.splice(0, 1, items[items.length - 1]);
+      items.splice(items.length - 1, 1, elements[0]);
+    }
+    const result = parseInt(items.join(''), 10);
+    return result;
 }
